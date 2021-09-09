f65bf714171b239eb37d761bc30f0b0ff959dc02
tchow <tchow@tutamail.com>
2021-09-09 11:21:45 -0400

    2-literal watching works

--- lazy-dpll.py
+++ lazy-dpll.py
@@ -1,7 +1,3 @@
-import sys
-import collections
-
-
 SAT = True
 UNSAT = False
 
@@ -39,42 +35,45 @@ def setup(raw_formula):
         ref_a = clause[0]
         ref_b = clause[-1]
         variables |= set(abs(lit) for lit in clause)
-    formula.append([clause, ref_a, ref_b])
+        formula.append([clause, ref_a, ref_b])
     nb_vars = len(variables)
     assignment = [0] * nb_vars
-    var_map = {var:i for i, var in enumerate(variables)}
-    return formula, assignment, var_map
+    variable_map = {var:i for i, var in enumerate(variables)}
+    return formula, assignment, variable_map
 
-def is_unit(ref_a, ref_b):
-    return ref_a == ref_b
 
 def unit_literals(formula):
     units = set()
     for (clause, ref_a, ref_b) in formula:
-        if is_unit(ref_a, ref_b):
+        if ref_a == ref_b:
             units.add(ref_a)
     return units
 
-def is_consistent(container):
-    for lit in container:
-        if -lit in container:
+def is_consistent(literals):
+    for lit in literals:
+        if -lit in literals:
             return False
     return True
 
-def choose_literal(formula):
+def is_unassigned(lit, assignment, variable_map):
+    var = abs(lit)
+    i = variable_map[var]
+    return assignment[i] == 0
+
+def choose_literal(formula, assignment, variable_map):
     for (clause, ref_a, ref_b) in formula:
-       return ref_a
+        if is_unassigned(ref_a, assignment, variable_map):
+            return ref_a
+        if is_unassigned(ref_b, assignment, variable_map):
+            return ref_b
     return None
 
-def update_ref(clause, assignment, var_map, ref, units):
+def update_ref(clause, assignment, variable_map, units, ref):
     if ref in units or -ref in units:
         for lit in clause:
-            var = abs(lit)
-            i = var_map[var]
-            if assignment[i] == 0:
-                ref = lit
-        if ref in [lit, -lit]:
-            ref = None
+            if is_unassigned(lit, assignment, variable_map):
+                return lit
+        return None
     return ref
 
 def is_sat(formula):
@@ -86,64 +85,70 @@ def is_unsat(formula):
             return True
     return False
 
-def unit_propagation(formula, assignment, var_map, units):
-    modified_formula = []
-    for (clause, ref_a, ref_b) in formula:
-        ref_a = update_ref(clause, assignment, var_map, ref_a, units)
-        ref_b = update_ref(clause[::-1], assignment, var_map, ref_b, units)
-        if ref_a and ref_b:
-            modified_clause = [clause, ref_a, ref_b]
-            modified_formula.append(modified_clause)
-        else:
-            modified_formula.append([[], ref_a, ref_b])
-    return modified_formula
+import time 
+def unit_propagation(formula, assignment, variable_map, units):
+    if is_consistent(units):
+        modified_assignment = assign_units(assignment, units)
+        modified_formula = []
+        for (clause, ref_a, ref_b) in formula:
+            if ref_a in units or ref_b in units:
+                continue
+            else:
+                ref_a = update_ref(clause, modified_assignment, variable_map, units, ref_a)
+                ref_b = update_ref(clause[::-1], modified_assignment, variable_map, units, ref_b)
+                if ref_a and ref_b:
+                    modified_clause = [clause, ref_a, ref_b]
+                    modified_formula.append(modified_clause)
+                else:
+                    modified_formula.append([[], ref_a, ref_b])
+        return modified_formula, modified_assignment
+    return formula, assignment
 
-def assign(assignment, units):
+def assign_units(assignment, units):
+    modified_assignment = assignment[:]
     for lit in units:
         var = abs(lit)
-        i = var_map[var]
-        assignment[i] = lit//var
-    return assignment
+        i = variable_map[var]
+        modified_assignment[i] = 1 if lit > 0 else -1
+    return modified_assignment 
 
-def dpll(formula, assignment, var_map):
+def dpll(formula, assignment, variable_map):
     if is_sat(formula):
         return SAT, assignment
     elif is_unsat(formula):
         return UNSAT
-    else: # unresolved
-        # unit prop
-        units = unit_literals(formula)
+    # unit prop
+    units = unit_literals(formula)
+    while units:
         if not is_consistent(units):
             return UNSAT
-        while units:
-            modified_assignment = assign(assignment, units)
-            formula = unit_propagation(formula, modified_assignment, var_map, units) 
-            units = unit_literals(formula)
-            if is_unsat(formula):
-                return UNSAT
-        # pure literal elim
-        # branch
-        lit = choose_literal(formula)
-        if lit:
-            return dpll(formula+[[0, [lit], lit, lit]], assignment, var_map) \
-                or dpll(formula+[[0, [-lit], -lit, -lit]], assignment, var_map)
-        return dpll(formula, assignment, var_map)
+        formula, assignment = unit_propagation(formula, assignment, variable_map, units) 
+        if is_unsat(formula):
+            return UNSAT
+        units = unit_literals(formula)
+    # pure literal elim
+    lit = choose_literal(formula, assignment, variable_map)
+    if lit:
+        return dpll(formula+[[[lit], lit, lit]], assignment, variable_map) \
+            or dpll(formula+[[[-lit], -lit, -lit]], assignment, variable_map)
+    return dpll(formula, assignment, variable_map)
 
 if __name__ == "__main__":
     tests = [
-        # ("sat_quinn.cnf", SAT),
+        ("sat_choueiry.cnf", SAT),
+        ("sat_quinn.cnf", SAT),
         ("sat_simple_v3_c2.cnf", SAT),
-        ("sat_simple_v3_c3.cnf", SAT),
-        # ("unsat_maf.cnf", UNSAT)
+        ("unsat_simple.cnf", UNSAT),
+        ("unsat_maf.cnf", UNSAT)
     ]
     for (dimacs_file, sat) in tests:
         raw_formula = parse("tests/" + dimacs_file)
-        formula, assignment, var_map = setup(raw_formula)
-        res = dpll(formula, assignment, var_map)
+        formula, assignment, variable_map = setup(raw_formula)
+        res = dpll(formula, assignment, variable_map)
         if res:
             sat, assignment = res
-            for var in var_map:
-                i = var_map[var]
+            for var in variable_map:
+                i = variable_map[var]
                 assignment[i] *= var
             print(assignment)
         print(res)
